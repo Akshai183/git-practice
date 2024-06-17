ed6952337609b6d7a9b9cab28c5502144239dba3
Akshai <akshay05pakhanati@gmail.com>
2024-06-17 23:31:15 +0530

    added 2.0.0 Major version

--- releases/1.0.0/1.3.1/app.py
+++ releases/1.0.0/1.3.1/app.py
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 print("This is my 1.3.1 version")
+=======
+print("This is verion 1.3.1")
+>>>>>>> de22059 (added 2.0.0 Major version)
--- releases/app.py
+++ releases/app.py
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 print("This is my release version")
+=======
+print("This is version 2.0.0")
+>>>>>>> de22059 (added 2.0.0 Major version)
