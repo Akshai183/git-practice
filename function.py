8b13cfe30dc862c165cb84f8d034e32ac43a7723
Akshai183 <109257185+Akshai183@users.noreply.github.com>
2024-12-04 06:00:59 +0000

    On branch main  Your branch is ahead of 'origin/main' by 2 commits. 	new file:   function.py

---
+++ function.py
@@ -0,0 +1,18 @@
+num1 = int(input("Enter your num1: "))
+num2 = int(input("Enter your num2: "))
+
+def addition():
+    add = num1 + num2 
+    print(add)
+
+def substraction():
+    sub = num1 - num2 
+    print(sub)
+    
+def multiplication():
+    mul = num1 * num2 
+    print(mul)
+
+addition()
+substraction()
+multiplication()
