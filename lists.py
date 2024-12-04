306d6cbb04afef243070b261ec36e3a33ff0d954
Akshai183 <109257185+Akshai183@users.noreply.github.com>
2024-12-04 09:09:19 +0000

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is ahead of 'origin/main' by 4 commits.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   lists.py

--- lists.py
+++ lists.py
@@ -1,3 +1,6 @@
 students_names = ["Akshai", "Uday", "Ram", "Jhon"]
-
+students_names.append("Jhon")
+students_names.remove("Jhon")
+students_names.extend(["Arjun", "Ravi", "Sita"])  # Add multiple values
 print(students_names)
+
