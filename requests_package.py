02ecc7a97f8349082082eae754e096c46013213e
Akshai183 <109257185+Akshai183@users.noreply.github.com>
2024-12-04 10:22:07 +0000

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is ahead of 'origin/main' by 10 commits.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   requests_package.py

--- requests_package.py
+++ requests_package.py
@@ -3,3 +3,7 @@ import requests
 response = requests.get("https://api.github.com/repos/kubernetes/kubernetes/pulls")
 
 print(response)
+
+output = response.json()
+for element in range(len(output)):
+    print(output[element]["user"]["login"])
